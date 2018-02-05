394d1e9e57499fe12e65cbb8a57ffbd47a3b19ba
mrkidney <azweig@stanford.edu>
2018-02-05 11:31:30 -0800

    different scalability

--- gae/gae/optimizer.py
+++ gae/gae/optimizer.py
@@ -12,7 +12,8 @@ class OptimizerVAE(object):
         sample = tf.reshape(sample, [-1])
         sample = tf.stop_gradient(sample)
 
-        self.cost = norm * tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=preds_sub * sample, targets=labels_sub * sample, pos_weight=pos_weight))
+        self.cost = norm * tf.reduce_mean(sample * tf.nn.weighted_cross_entropy_with_logits(logits=preds_sub, targets=labels_sub, pos_weight=pos_weight))
+        self.cost *= 1.0 * num_nodes * num_nodes / tf.reduce_sum(sample)
 
         self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)  # Adam Optimizer
 
@@ -20,8 +21,10 @@ class OptimizerVAE(object):
         self.log_lik = self.cost
 
         if FLAGS.vae:
-            self.kl = (0.5 / num_nodes) * tf.reduce_mean(tf.reduce_sum(1 + 2 * model.z_log_std * model.sample - tf.square(model.z_mean * model.sample) - tf.square(tf.exp(model.z_log_std * model.sample)), 1))
+            self.kl = (0.5 / num_nodes) * tf.reduce_mean(model.sample * tf.reduce_sum(1 + 2 * model.z_log_std - tf.square(model.z_mean) - tf.square(tf.exp(model.z_log_std)), 1))
+            self.kl *= 1.0 * num_nodes / tf.reduce_sum(model.sample)
             self.cost -= self.kl
+
 
         self.opt_op = self.optimizer.minimize(self.cost)
         self.grads_vars = self.optimizer.compute_gradients(self.cost)

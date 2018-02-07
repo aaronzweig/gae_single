de4a6d00a91860e1bb5a895cf80a7ddf51434d5a
mrkidney <azweig@stanford.edu>
2018-02-07 10:26:47 -0800

    terror mode activated you dumbass bitch

--- gae/gae/model.py
+++ gae/gae/model.py
@@ -159,7 +159,7 @@ class GCNModelFeedback(GCNModelVAE):
         update = l2((update, recon, z))
 
         #update = (1 - FLAGS.autoregressive_scalar) * z + FLAGS.autoregressive_scalar * update
-        lamb = tf.Variable(0, name = 'scalar')
+        lamb = tf.Variable(0., name = 'scalar')
         update = (1 - tf.nn.sigmoid(lamb)) * z + tf.nn.sigmoid(lamb) * update
 
         reconstructions = l3(update)

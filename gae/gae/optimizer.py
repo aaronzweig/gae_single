4a5d18ea7a447cec7349d32fc548dada1827f837
mrkidney <azweig@stanford.edu>
2018-05-09 18:26:27 -0700

    less stupid monte carlo sampling?

--- gae/gae/optimizer.py
+++ gae/gae/optimizer.py
@@ -12,8 +12,9 @@ class OptimizerVAE(object):
         if FLAGS.subsample:
             edge_count = tf.count_nonzero(labels_sub)
             edge_indices = tf.where(tf.not_equal(labels_sub, 0))
-            no_edge_indices = tf.where(tf.equal(labels_sub, 0))
-            no_edge_indices = tf.random_shuffle(no_edge_indices)[:edge_count]
+            # no_edge_indices = tf.where(tf.equal(labels_sub, 0))
+            # no_edge_indices = tf.random_shuffle(no_edge_indices)[:edge_count]
+            no_edge_indices = tf.random_uniform(edge_count, maxval = num_nodes*num_nodes)
             self.cost = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=tf.gather(preds_sub, edge_indices), targets=tf.gather(labels_sub, edge_indices), pos_weight=1))
             self.cost += tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=tf.gather(preds_sub, no_edge_indices), targets=tf.gather(labels_sub, no_edge_indices), pos_weight=1))
         else:

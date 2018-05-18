72a96c166100f996804e85675ec216230e8807fe
mrkidney <azweig@stanford.edu>
2018-05-17 23:45:51 -0700

    sub_frac

--- gae/gae/optimizer.py
+++ gae/gae/optimizer.py
@@ -14,8 +14,7 @@ class OptimizerVAE(object):
             edge_indices = tf.where(tf.not_equal(labels_sub, 0))
             # no_edge_indices = tf.where(tf.equal(labels_sub, 0))
             # no_edge_indices = tf.random_shuffle(no_edge_indices)[:edge_count]
-            no_edge_count = tf.count_nonzero(labels_sub, keepdims = True)
-            no_edge_count *= FLAGS.subsample_frac
+            no_edge_count = FLAGS.subsample_frac * tf.count_nonzero(labels_sub, keepdims = True)
             no_edge_indices = tf.random_uniform(no_edge_count, maxval = num_nodes*num_nodes, dtype=tf.int32)
             self.cost = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=tf.gather(preds_sub, edge_indices), targets=tf.gather(labels_sub, edge_indices), pos_weight=1))
             self.cost += tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=tf.gather(preds_sub, no_edge_indices), targets=tf.gather(labels_sub, no_edge_indices), pos_weight=1))
